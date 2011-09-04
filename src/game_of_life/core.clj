40575e8aff82fb371c7ccff73cfa11eda8c3a6be
Blake Smith <blakesmith0@gmail.com>
2011-09-04 16:04:14 -0500

    Basic two neighbor alive check

--- src/game_of_life/core.clj
+++ src/game_of_life/core.clj
@@ -1,9 +1,23 @@
 (ns game-of-life.core)
 
+(def neighbor-cords
+  [[-1 0]
+   [1 0]])
+
 (defn cell-at [loc world]
   (let [[x y] loc
         alive (nth (nth world y) x)]
     {:alive alive :x x :y y}))
 
+(defn find-neighbors [cell world]
+  (let [x (cell :x) y (cell :y)]
+    (map
+      (fn [cord]
+        (cell-at [(+ x (first cord)) (+ y (second cord))]
+                 world))
+      neighbor-cords)))
+
 (defn alive-next? [cell world]
-  false)
+  (<= 2
+    (count
+      (filter #(= true (% :alive)) (find-neighbors cell world)))))
--- test/game_of_life/test/core.clj
+++ test/game_of_life/test/core.clj
@@ -7,5 +7,9 @@
     (is (= {:alive true :x 1 :y 0} (cell-at [1 0] world)))))
 
 (deftest not-alive-if-fewer-than-two-neighbors
-  (let [world [[false true false]]]
+  (let [world [[false true true]]]
     (is (= false (alive-next? (cell-at [1 0] world) world)))))
+
+(deftest alive-if-two-neighbors-alive
+  (let [world [[true true true]]]
+    (is (= true (alive-next? (cell-at [1 0] world) world)))))
